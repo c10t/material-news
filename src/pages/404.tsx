271d0be36903b343cfd8da30d70adefb741cd643
c10t <c10t@fa.ke>
2019-07-24 09:53:43 +0900

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+const PageNotFoundPage: React.FC = () => (<>404 Page Not Found</>)
+
+export default PageNotFoundPage
--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 
-import configureStore from './src/store/configure-store'
+import store from './src/store'
 
 export default ({ element }) => {
-  const store = configureStore()
   return <Provider store={store}>{element}</Provider>
 }
