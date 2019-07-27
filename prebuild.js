bda11fbc4cdbb7a1c7f7921744bc7b16e734eed0
c10t <c10t@fa.ke>
2019-07-27 11:11:52 +0900

    fetch news before build

--- prebuild.js
+++ prebuild.js
@@ -1,1 +1,27 @@
+const axios = require('axios')
+const fs = require('fs').promises
+
+const BASE_URL = 'https://hacker-news.firebaseio.com/v0'
+const topstories = () => `${BASE_URL}/topstories.json`
+const item = id => `${BASE_URL}/item/${id}.json`
+
+const readAndWriteItem = async id => {
+  await axios.get(item(id)).then(res => res.data).then(data => console.log(data.title, data.score))
+}
+
+const main = async () => {
+  try {
+    const stories = await axios.get(topstories())
+    const top10 = stories.data.slice(0, 10)
+    await Promise.all(top10.map(id => readAndWriteItem(id)))
+  } catch (err) {
+    throw err
+  }
+}
+
 console.log(`prebuild script running from ${__dirname}`)
+main().catch(err => {
+  console.log('--- caught an error ---')
+  console.log(err)
+  process.exitCode = 1
+})
